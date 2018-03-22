28fedb2a6fc7218c699b9524974eb3d68f41199c
Matthew Smith <matthew.smith@thinkinglogic.com>
2018-03-22 07:36:03 +0000

    [MS] Removed active code from index.js, added package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
 import MapContainer from './components/mapContainer';
 
+import  "./assets/scss/main.scss";
 
-
-
-
-const onError = function(msg) {
-    alert(msg);
-}
-
-function onSave(result) {
-    console.log("onSave invoked. result=" + JSON.stringify(result));
-}
-function onCancel() {
-    console.log("onCancel invoked.");
-}
-
-
-
-ReactDOM.render(
-    <div id="example-component">
-        <MapContainer onCancel={onCancel} onSave={onSave} onError={onError} data={{}} />
-    </div>, 
-    document.getElementById('app'));
-
+export default MapContainer;
